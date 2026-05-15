a53d4ceb9d91e8537ac2e0769f2c2130c5f5f508
49895963 <b26124yn@aiit.ac.jp>
2026-05-15 04:50:35 +0000

    Add admin user delete feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_13_074239) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_15_044329) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_13_074239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.boolean "admin"
   end
 
 end
