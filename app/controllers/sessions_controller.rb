8fa4a694273901e998dc9c19fea124a6c936413b
49895963 <b26124yn@aiit.ac.jp>
2026-05-18 05:13:14 +0000

    Add account activation feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
         log_in user
         params[:session][:remember_me] == "1" ? remember(user) : forget(user)
         redirect_to user
-      el　se
+      else
         flash[:warning] = "Account not activated. Check your email for the activation link."
         redirect_to root_url
       end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,5 @@ Rails.application.configure do
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :test
 config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-config.action_controller.forgery_protection_origin_check = falseend
+config.action_controller.forgery_protection_origin_check = false
+end
