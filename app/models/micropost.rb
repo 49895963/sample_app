458dca28e637bc878a4e71a55cf0641433e515ec
49895963 <b26124yn@aiit.ac.jp>
2026-05-19 07:57:39 +0000

    Validate micropost images

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,19 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :acceptable_image
+  private
+
+    def acceptable_image
+      return unless image.attached?
+
+      unless image.blob.byte_size <= 5.megabytes
+        errors.add(:image, "should be less than 5MB")
+      end
+
+      acceptable_types = ["image/jpeg", "image/gif", "image/png"]
+      unless acceptable_types.include?(image.content_type)
+        errors.add(:image, "must be a JPEG, GIF, or PNG")
+      end
+    end
 end
