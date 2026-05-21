1c8ecbfe417c9375a23cba41e63b188f87f94b17
49895963 <b26124yn@aiit.ac.jp>
2026-05-21 13:54:33 +0000

    Add follow and unfollow methods

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,33 @@ class UserTest < ActiveSupport::TestCase
       user.destroy
     end
   end
+  test "should follow and unfollow a user" do
+    michael = User.create!(
+      name: "Michael Example",
+      email: "michael-follow@example.com",
+      password: "password",
+      password_confirmation: "password",
+      activated: true,
+      activated_at: Time.zone.now
+    )
+
+    archer = User.create!(
+      name: "Sterling Archer",
+      email: "archer-follow@example.com",
+      password: "password",
+      password_confirmation: "password",
+      activated: true,
+      activated_at: Time.zone.now
+    )
+
+    assert_not michael.following?(archer)
+
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
 end
